2974b871ab2a0e4761fed9402c8c60602a32b817
Liubou Chaika <liubouchaika@gmail.com>
2020-03-18 22:03:40 +0300

    feat: add carbon-dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,13 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+  let number = Number(sampleActivity);
+  if(isNaN(number) || number>15 || number<1 || typeof sampleActivity !== 'string') {
+  	return false;
+  } else {
+  	let year = Math.log(MODERN_ACTIVITY / number) / (0.693 / HALF_LIFE_PERIOD);
+  	let roundedYear = Math.ceil(year);
+  	return roundedYear
+  }
+}
