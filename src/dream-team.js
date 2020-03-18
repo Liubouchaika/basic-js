e8dc98972ea27ce55e0ed44f9d04cadc3781fe72
Liubou Chaika <liubouchaika@gmail.com>
2020-03-18 23:26:43 +0300

    feat:add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	if(Array.isArray(members)) {
+		let rightType = members.filter(item => {
+  	return typeof item == 'string';
+  }); 
+	  let noSpaces = rightType.map(item => item.split(' ').join('')); 
+	  let names = noSpaces.map(item => item[0].toUpperCase()); 
+	  let logo = names.sort().join('');
+	  return logo;
+	} else {
+		return false;
+	}
 };
