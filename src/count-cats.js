a86f8d08699937bf1650f37d4e5a3e5c135bb683
Liubou Chaika <liubouchaika@gmail.com>
2020-03-18 22:22:02 +0300

    fix: edit count-cats function

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,7 +2,18 @@ module.exports = function countCats(back) {
 	let arr = back.flat(Infinity); 
 	let count = 0; 
 	arr.forEach(item => {
-		count = count + (item.split('^^').length - 1);
+		if(item =='^^') {
+			count=count+1;
+		};
 	}); 
 	return count;
 };
+
+/*function countCats(back) { 
+	let arr = back.flat(Infinity); 
+	let count = 0; 
+	arr.forEach(item => {
+		count = count + (item.split('^^').length - 1);
+	}); 
+	return count;
+};*/
