73a50fb5250f91563de3b7ba99f317030f0d36f7
Liubou Chaika <liubouchaika@gmail.com>
2020-03-19 22:35:26 +0300

    fix: edit hanoi-tower function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,8 +1,10 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed ) {
-    let turns = Math.pow(2, disksNumber)-1;
-    let seconds = turns/turnsSpeed*60*60;
-    let object = {};
-  	object.turns = turns;
-   	object.seconds = seconds;
+    let turns = 2 ** disksNumber - 1;
+    let seconds = turns / (turnsSpeed / (60 * 60));
+
+    let object = {
+    	turns: turns,
+    	seconds: seconds
+    };
     return object;
 }
