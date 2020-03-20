68d834fac92602e8f32b44cb67214807a2ec6613
Liubou Chaika <liubouchaika@gmail.com>
2020-03-20 21:07:38 +0300

    fix: edit carbor-dating function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,12 +2,20 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  let number = Number(sampleActivity);
-  if(isNaN(number) || number>15 || number<1 || typeof sampleActivity !== 'string') {
+  let number = parseFloat(sampleActivity);
+  if(isNaN(number) || number>MODERN_ACTIVITY || number<1 || typeof sampleActivity !== 'string') {
   	return false;
   } else {
   	let year = Math.log(MODERN_ACTIVITY / number) / (0.693 / HALF_LIFE_PERIOD);
   	let roundedYear = Math.ceil(year);
   	return roundedYear
   }
+  //  if (typeof sample !== "string" || //isn't a string
+  //     /[A-Za-z]/.test(sample) || //has letters
+  //     sample < 1 || sample > MODERN_ACTIVITY) //is inadequate
+  // return false;
+
+  // let rate = Math.LN2.toFixed(3) / HALF_LIFE_PERIOD;
+  // let years = Math.log(MODERN_ACTIVITY / parseFloat(sample)) / rate;
+  // return Math.ceil(years);
 }
