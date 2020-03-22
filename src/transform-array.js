94e3d9bb8dadf88dbda29d1ac9367a14ac868e93
Liubou Chaika <liubouchaika@gmail.com>
2020-03-22 15:12:07 +0300

    feat: add transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,36 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+   let discardNext = '--discard-next';
+   let discardPrev = '--discard-prev';
+   let doubleNext = '--double-next';
+   let doublePrev = '--double-prev';
+   let newArray =[];
+ 
+	if(!Array.isArray(arr)) {
+   		throw new Error("It's not array!");
+	};
+
+   	for (let i=0; i<arr.length; i++) {
+   	 	if(arr[i] == discardNext) {
+   	 		if (i+1 !== arr.length) {
+   	 			i++;
+   	 		}
+   	 	} else if(arr[i] == discardPrev) {
+   	 		if(i !== 0) {
+   	 			newArray.pop();
+   	 		}
+   	 	} else if(arr[i] == doubleNext) {
+   	 		if(i+1 !== arr.length) {
+   	 			newArray.push(arr[i+1]);
+   	 		}
+   	 	} else if(arr[i] == doublePrev) {
+   	 		if(i !== 0) {
+   	 			newArray.push(arr[i-1]);
+   	 		} 
+   	 	} else {
+   	 		newArray.push(arr[i]);
+   	 	}
+   	}
+
+   	return newArray;
+
 };
