f33bb6fadb0afe759ca4a6eb32701d58bc1e3f66
Liubou Chaika <liubouchaika@gmail.com>
2020-03-22 22:40:39 +0300

    feat:add extended-repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,20 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+   let arrAdditions = new Array(options.additionRepeatTimes);
+   str = str + '';
+
+   let addition = undefined;
+   if (options.addition !== undefined) {
+   		addition = '' + options.addition;
+   }
+
+   let additionsSrt = str + arrAdditions.fill(addition).join(options.additionSeparator);
+   let arrAdditionsSrt = new Array(options.repeatTimes);
+   let result;
+
+   if (options.separator == undefined) {
+   	options.separator = '+';
+   	}
+   	result = arrAdditionsSrt.fill(additionsSrt).join(options.separator);
+   	return result;
 };
   
